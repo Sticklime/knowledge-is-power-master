2236fe349730c48523a4ec7f2f74d7f1ab0e2356
Sticklime <91189001+Sticklime@users.noreply.github.com>
2023-06-26 10:10:54 +0300

    Main

---
+++ Assets/CodeBase/Infrastructure/AssetManagement/AssetPath.cs
@@ -0,0 +1,9 @@
+namespace CodeBase.Infrastructure.AssetManagement
+{
+  public static class AssetPath
+  {
+    public const string HeroPath = "Hero/hero";
+    public const string Loot = "Loot/Loot";
+    public const string HudPath = "Hud/Hud";
+  }
+}
---
+++ Assets/CodeBase/Infrastructure/Factory/GameFactory.cs
@@ -0,0 +1,122 @@
+using System.Collections.Generic;
+using CodeBase.Enemy;
+using CodeBase.Infrastructure.AssetManagement;
+using CodeBase.Logic;
+using CodeBase.Services.PersistentProgress;
+using CodeBase.Services.Randomizer;
+using CodeBase.Services.StaticData;
+using CodeBase.StaticData;
+using CodeBase.UI;
+using UnityEngine;
+using UnityEngine.AI;
+using Object = UnityEngine.Object;
+
+namespace CodeBase.Infrastructure.Factory
+{
+  public class GameFactory : IGameFactory
+  {
+    public List<ISavedProgressReader> ProgressReaders { get; } = new List<ISavedProgressReader>();
+    public List<ISavedProgress> ProgressWriters { get; } = new List<ISavedProgress>();
+
+    private readonly IAssetProvider _assets;
+    private readonly IStaticDataService _staticData;
+    private readonly IRandomService _randomService;
+    private readonly IPersistentProgressService _persistentProgressService;
+    private GameObject _heroGameObject;
+
+    public GameFactory(IAssetProvider assets, IStaticDataService staticData, IRandomService randomService, IPersistentProgressService persistentProgressService)
+    {
+      _assets = assets;
+      _staticData = staticData;
+      _randomService = randomService;
+      _persistentProgressService = persistentProgressService;
+    }
+
+    public GameObject CreateHero(GameObject at) =>
+      _heroGameObject = InstantiateRegistered(AssetPath.HeroPath, at.transform.position);
+
+    public GameObject CreateHud()
+    {
+      GameObject hud = InstantiateRegistered(AssetPath.HudPath);
+      
+      hud.GetComponentInChildren<LootCounter>()
+        .Construct(_persistentProgressService.Progress.WorldData);
+      
+      return hud;
+    }
+
+    public LootPiece CreateLoot()
+    {
+      LootPiece lootPiece = InstantiateRegistered(AssetPath.Loot)
+        .GetComponent<LootPiece>();
+      
+      lootPiece.Construct(_persistentProgressService.Progress.WorldData);
+      
+      return lootPiece;
+    }
+
+    public GameObject CreateMonster(MonsterTypeId typeId, Transform parent)
+    {
+      MonsterStaticData monsterData = _staticData.ForMonster(typeId);
+      GameObject monster = Object.Instantiate(monsterData.Prefab, parent.position, Quaternion.identity, parent);
+
+      IHealth health = monster.GetComponent<IHealth>();
+      health.Current = monsterData.Hp;
+      health.Max = monsterData.Hp;
+
+      monster.GetComponent<ActorUI>().Construct(health);
+      monster.GetComponent<NavMeshAgent>().speed = monsterData.MoveSpeed;
+
+      Attack attack = monster.GetComponent<Attack>();
+      attack.Construct(_heroGameObject.transform);
+      attack.Damage = monsterData.Damage;
+      attack.Cleavage = monsterData.Cleavage;
+      attack.EffectiveDistance = monsterData.EffectiveDistance;
+
+      monster.GetComponent<AgentMoveToPlayer>()?.Construct(_heroGameObject.transform);
+      monster.GetComponent<RotateToHero>()?.Construct(_heroGameObject.transform);
+
+      LootSpawner lootSpawner = monster.GetComponentInChildren<LootSpawner>();
+      lootSpawner.Construct(this, _randomService);
+      lootSpawner.SetLootValue(monsterData.MinLootValue, monsterData.MaxLootValue);
+
+      return monster;
+    }
+
+    public void Register(ISavedProgressReader progressReader)
+    {
+      if (progressReader is ISavedProgress progressWriter)
+        ProgressWriters.Add(progressWriter);
+
+      ProgressReaders.Add(progressReader);
+    }
+
+    public void Cleanup()
+    {
+      ProgressReaders.Clear();
+      ProgressWriters.Clear();
+    }
+
+    private GameObject InstantiateRegistered(string prefabPath, Vector3 at)
+    {
+      GameObject gameObject = _assets.Instantiate(path: prefabPath, at: at);
+      RegisterProgressWatchers(gameObject);
+
+      return gameObject;
+    }
+
+    private GameObject InstantiateRegistered(string prefabPath)
+    {
+      GameObject gameObject = _assets.Instantiate(path: prefabPath);
+      RegisterProgressWatchers(gameObject);
+
+      return gameObject;
+    }
+
+    private void RegisterProgressWatchers(GameObject gameObject)
+    {
+      foreach (ISavedProgressReader progressReader in gameObject.GetComponentsInChildren<ISavedProgressReader>())
+        Register(progressReader);
+    }
+  }
+}
